c0413b2b6d83fc7ab64cbbb39e94aa4f3d964b46
Parallax18 <rhythmist204@gmail.com>
2024-11-17 19:30:57 +0100

    fix: auto fill and button pulse

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[2.5rem] text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-neutral-300",
@@ -43,10 +44,12 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button";
+    const Comp = asChild ? Slot : motion.button;
     return (
+      // @ts-expect-error - motion type mis-match -> TODO: fix this
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
+        whileTap={{ scale: 0.98 }}
         ref={ref}
         {...props}
       />
